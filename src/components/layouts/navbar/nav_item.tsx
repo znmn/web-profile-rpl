015df1d0ab9492427490b99cbfa7752c8537d363
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2025-01-26 12:08:42 +0700

    refactor(navigation): add selected navigation item style

--- src/components/layouts/navbar/nav_item.tsx
+++ src/components/layouts/navbar/nav_item.tsx
@@ -1,6 +1,8 @@
 import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useMemo } from "react";
 
 type NavItemProps = {
   icon: IconDefinition;
@@ -9,16 +11,28 @@ type NavItemProps = {
 };
 
 export const NavItem = (props: NavItemProps) => {
+  const path = usePathname();
+
+  const selected = useMemo(() => path === props.route, [path]);
+
   return (
     <Link
       href={props.route}
-      className=" px-5 py-3 flex flex-row lg:justify-start items-center gap-2 w-full lg:w-fit rounded-full group hover:bg-primary/90 dark:group-hover:bg-primary/20 duration-300 "
+      className={`px-5 py-3 flex flex-row lg:justify-start items-center gap-2 w-full lg:w-fit rounded-full group ${
+        selected && "bg-primary/90 dark:bg-primary/20"
+      } hover:bg-primary/90 dark:hover:bg-primary/20 duration-300 `}
     >
       <FontAwesomeIcon
         icon={props.icon}
-        className="w-4 lg:w-5 h-4 lg:h-5 text-gray-800 dark:text-gray-200 group-hover:text-gray-200 duration-300"
+        className={`w-4 lg:w-5 h-4 lg:h-5  dark:text-gray-200 ${
+          selected ? "text-gray-200" : "text-gray-800"
+        } group-hover:text-gray-200 duration-300`}
       />
-      <p className="font-medium text-sm lg:text-md text-gray-800 dark:text-gray-200 group-hover:text-gray-200 duration-300">
+      <p
+        className={`font-medium text-sm lg:text-md  dark:text-gray-200 ${
+          selected ? "text-gray-200" : "text-gray-800"
+        } group-hover:text-gray-200 duration-300`}
+      >
         {props.name}
       </p>
     </Link>
