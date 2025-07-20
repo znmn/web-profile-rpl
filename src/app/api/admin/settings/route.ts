2a73b583a1bfd2b06ec8b9a1d0dce8c756493f2f
Zainul Muhaimin <zain.muhaimin01@gmail.com>
2025-07-21 06:21:22 +0700

    fix(route): replace Response with NextResponse for consistent response handling

--- src/app/api/admin/settings/route.ts
+++ src/app/api/admin/settings/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { getSettings, updateSettings } from "@/controllers/settingControllers";
 
 import { fetchedAll, serverError, updated, requiredOptional } from "./config";
@@ -6,9 +6,9 @@ import { fetchedAll, serverError, updated, requiredOptional } from "./config";
 export async function GET(req: NextRequest) {
 	try {
 		const settings = await getSettings();
-		return Response.json({ ...fetchedAll, ...settings }, { status: 200 });
+		return NextResponse.json({ ...fetchedAll, ...settings }, { status: 200 });
 	} catch (e) {
-		return Response.json(serverError, { status: 500 });
+		return NextResponse.json(serverError, { status: 500 });
 	}
 }
 
@@ -17,14 +17,14 @@ export async function PATCH(req: NextRequest) {
 		let data = await req.json();
 
 		const settings = await updateSettings(data);
-		return Response.json({ ...updated, ...settings }, { status: 200 });
+		return NextResponse.json({ ...updated, ...settings }, { status: 200 });
 	} catch (e: any) {
 		if (e instanceof SyntaxError) {
-			return Response.json({ success: false, message: "Wrong JSON Body Format" }, { status: 400 });
+			return NextResponse.json({ success: false, message: "Wrong JSON Body Format" }, { status: 400 });
 		}
 		if (e.message.toLowerCase().includes("no data")) {
-			return Response.json(requiredOptional, { status: 400 });
+			return NextResponse.json(requiredOptional, { status: 400 });
 		}
-		return Response.json(serverError, { status: 500 });
+		return NextResponse.json(serverError, { status: 500 });
 	}
 }
